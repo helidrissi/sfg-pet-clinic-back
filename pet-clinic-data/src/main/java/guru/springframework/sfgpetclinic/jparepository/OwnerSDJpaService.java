ee5676119bd00e856248f3907c5345560fd0f064
helidrissi <hamza.elidrissi@mistral.fr>
2020-11-26 19:16:46 +0100

    26/11/2020

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/jparepository/OwnerSDJpaService.java
@@ -0,0 +1,66 @@
+package guru.springframework.sfgpetclinic.jparepository;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("SpringData")
+public class OwnerSDJpaService implements OwnerService {
+
+    private OwnerRepository ownerRepository;
+    private PetRepository petRepository;
+    private VetRepository vetRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository, VetRepository vetRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastname) {
+        return ownerRepository.findByLastName(lastname);
+    }
+
+    @Override
+    public Set<Owner> finAll() {
+
+        Set<Owner> owners= new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        Optional<Owner> owner=ownerRepository.findById(aLong);
+        if(owner.isPresent()){
+            return owner.get();
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+       ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+           ownerRepository.deleteById(aLong);
+    }
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetRepository.java
@@ -2,7 +2,9 @@ package guru.springframework.sfgpetclinic.repositories;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PetRepository extends CrudRepository<Pet,Long> {
 
 }
